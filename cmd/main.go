a41acc90854d65039856282c99be955a39e950b2
Ryan Faerman <ryan_faerman@ultimatesoftware.com>
2018-09-14 10:57:45 -0400

    add example to cmd

--- cmd/main.go
+++ cmd/main.go
@@ -5,15 +5,15 @@ import (
 
 	"github.com/ryanfaerman/calendar"
 	"github.com/ryanfaerman/calendar/gregorian"
+	"github.com/ryanfaerman/calendar/julian"
 )
 
 func main() {
 
-	g := gregorian.Date{1945, 11, 12}
-	abs := g.ToAbsolute()
-	fmt.Println(abs)
-	fmt.Println(gregorian.NewFromAbsolute(abs))
+	g := gregorian.Date{2018, 9, 15}
+	j := julian.Date{2018, 9, 2}
 
-	fmt.Println(calendar.Convert(g, gregorian.Date{}))
+	fmt.Println("gregorian -> julian", calendar.Convert(g, julian.Date{}))
+	fmt.Println("julian -> gregorian", calendar.Convert(j, gregorian.Date{}))
 
 }
